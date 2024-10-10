0b7326285fca0f243b791f79ebe6a91c95085611
dev csis <dev@csis.or.id>
2024-10-10 15:31:11 +0700

    bootstrapping the menu

--- app/web/url.py
+++ app/web/url.py
@@ -2,6 +2,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'web'
+
 urlpatterns = [
     path('dashboard/', views.index, name='index'),
     path('user/', views.userHome, name='index-user'),
