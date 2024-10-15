76ee0817513644b884a394a8a55c0c5e81f57004
dev csis <dev@csis.or.id>
2024-10-15 16:19:49 +0700

    custom dev and prod

--- core/settings/development.py
+++ core/settings/development.py
@@ -1,4 +1,4 @@
-from .base import *
+from .core import *
 
 DEBUG = True
 SECRET_KEY = config('SECRET_KEY')
--- core/settings/production.py
+++ core/settings/production.py
@@ -1,4 +1,4 @@
-from .base import *
+from .core import *
 
 DEBUG = False
 SECRET_KEY = config('SECRET_KEY')
